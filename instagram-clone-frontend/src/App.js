93a89a177b34d09acb17d5ead3ad5f1de727226f
unknown <alextq3036@gmail.com>
2022-01-04 23:03:27 -0500

    Created a header component with the instagram logo inside

--- instagram-clone-frontend/src/App.js
+++ instagram-clone-frontend/src/App.js
@@ -1,7 +1,15 @@
-import "./App.css";
+import "./styles/App.css";
+import Header from "./components/Header";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <Header />
+      {/* Header */}
+      {/* Post */}
+      {/* Post */}
+    </div>
+  );
 }
 
 export default App;
---
+++ instagram-clone-frontend/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/Header.css";
+
+function Header() {
+  return (
+    <div className="app__header">
+      <img
+        className="app__headerImage"
+        src="https://www.pinclipart.com/picdir/big/59-590993_follow-us-on-instagram-logo-png-clipart.png"
+        alt="instagram logo"
+      />
+    </div>
+  );
+}
+
+export default Header;
