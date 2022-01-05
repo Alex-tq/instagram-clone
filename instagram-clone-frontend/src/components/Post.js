8a4a76fa8c19f49e5afcc6b50ede468045184c6a
unknown <alextq3036@gmail.com>
2022-01-05 00:49:33 -0500

    Passing down props to Posts

--- instagram-clone-frontend/src/components/Post.js
+++ instagram-clone-frontend/src/components/Post.js
@@ -2,24 +2,18 @@ import React from "react";
 import "../styles/Post.css";
 import Avatar from "@mui/material/Avatar";
 
-function Post() {
+function Post({ username, caption, imgUrl, avatarUrl }) {
   return (
     <div className="post">
-      <Avatar
-        className="post_avatar"
-        alt="avatar"
-        src="https://images.unsplash.com/photo-1608889825205-eebdb9fc5806?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGF2YXRhcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"
-      />
-      <h3>username</h3>
+      <div className="post_header">
+        <Avatar className="post_avatar" alt="User avatar" src={avatarUrl} />
+        <h3>{username}</h3>
+      </div>
 
-      <img
-        src="https://images.unsplash.com/photo-1522069169874-c58ec4b76be5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZmlzaHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1000&q=60"
-        alt=""
-        className="post_image"
-      />
+      <img src={imgUrl} alt="" className="post_image" />
 
-      <p>
-        <strong>Username:</strong> this is a caption
+      <p className="caption">
+        <strong>{username} </strong> {`: ${caption}`}
       </p>
     </div>
   );
