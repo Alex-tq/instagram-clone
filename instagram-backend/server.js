1fd17e6f76ea0a3dad7ba4e1c78a77bd97232712
unknown <alextq3036@gmail.com>
2022-01-09 23:17:17 -0500

    Connected the server to the Mongodb database

--- instagram-backend/server.js
+++ instagram-backend/server.js
@@ -7,8 +7,18 @@ dotenv.config();
 
 const PORT = process.env.PORT || 8081;
 const app = express();
+const URI = process.env.MONGODB_URI;
 
 app.use(express.json());
+app.use(cors());
+
+mongoose.connect(URI, {
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.once("open", () => {
+  console.log("Connected to Database");
+});
 
 app.get("/", (req, res) => {
   res.status(200).send("Hello Instagram Clone");
