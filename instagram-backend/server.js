125726251c300301da3daa02a7d8521a66967eda
Alexander Tamarez <alextq3036@gmail.com>
2022-01-28 05:54:37 -0500

    Deployed buid

--- instagram-backend/server.js
+++ instagram-backend/server.js
@@ -32,7 +32,7 @@ app.use(session(sessionOptions));
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    origin: "https://instagram-clone-3e26f.firebaseapp.com",
   })
 );
 
